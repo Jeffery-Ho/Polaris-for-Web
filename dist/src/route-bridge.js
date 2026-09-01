d274e02a13481523f499d2d3638fe325533f289d
Hos <hexianji@outlook.com>
2026-09-01 17:45:31 +0800

    chore: include generated extension bundle

---
+++ dist/src/route-bridge.js
@@ -0,0 +1,20 @@
+(() => {
+  const ROUTE_CHANGE_EVENT = "polaris-for-web-route-change";
+
+  function notifyRouteChange() {
+    window.dispatchEvent(new Event(ROUTE_CHANGE_EVENT));
+  }
+
+  ["pushState", "replaceState"].forEach((method) => {
+    const original = window.history[method];
+    window.history[method] = function (...args) {
+      const result = original.apply(this, args);
+      notifyRouteChange();
+      return result;
+    };
+  });
+
+  window.addEventListener("popstate", notifyRouteChange);
+  window.addEventListener("hashchange", notifyRouteChange);
+
+})();
